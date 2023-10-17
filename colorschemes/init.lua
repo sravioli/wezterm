09e7186962a4d62f9df8cf845f8dbf85882db2e4
Simone Fidanza <fsimone2002@proton.me>
2023-10-17 10:05:13 +0200

    fix(colorschemes)!: rename `colors` folder to `colorshemes`

---
+++ colorschemes/init.lua
@@ -0,0 +1,7 @@
+return {
+  ["kanagawa"] = require "colorschemes.kanagawa",
+  ["catppuccin-mocha"] = require("colorschemes.catppuccin").mocha,
+  ["catppuccin-latte"] = require("colorschemes.catppuccin").latte,
+  ["catppuccin-macchiato"] = require("colorschemes.catppuccin").macchiato,
+  ["catppuccin-frappe"] = require("colorschemes.catppuccin").frappe,
+}
