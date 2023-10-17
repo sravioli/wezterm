b987dd2648bc7af3f82fbb4890b3cdb8d315ff89
Simone Fidanza <fsimone2002@proton.me>
2023-10-17 22:35:01 +0200

    feat(wezterm): load new config modules

--- wezterm.lua
+++ wezterm.lua
@@ -1,3 +1,8 @@
 local Config = require "config.init"
 
-return Config:init():add(require "config.appearance").options
+return Config:init()
+  :add(require "config.window")
+  :add(require "config.appearance")
+  :add(require "config.cursor")
+  :add(require "config.command-palette")
+  :add(require "config.bell").options
