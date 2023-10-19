93473fddc3cf854443259fe5256f5ed00178ba6a
Simone Fidanza <fsimone2002@proton.me>
2023-10-19 20:35:31 +0200

    feat(wezterm): load configs

--- wezterm.lua
+++ wezterm.lua
@@ -3,9 +3,10 @@ local Config = require "config.init"
 return Config:init()
   :add(require "config.gpu")
   :add(require "config.font")
+  :add(require "config.bell")
   :add(require "config.window")
   :add(require "config.cursor")
+  :add(require "config.general")
   :add(require "config.appearance")
-  :add(require "config.exit-behavior")
-  :add(require "config.bell").options
+  :add(require "config.exit-behavior").options
 -- :add(require "config.command-palette")
