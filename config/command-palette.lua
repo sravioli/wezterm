e606d501afaccd46a289af478a49d87f0d5e9d30
Simone Fidanza <fsimone2002@proton.me>
2023-10-19 08:53:19 +0200

    refactor(config:command-palette)!: move command palette and char select config to appearance file

--- config/command-palette.lua
+++ config/command-palette.lua
@@ -1,16 +1,4 @@
 ---@class config
----@field command_palette_bg_color string Specifies the background color used by
----       `ActivateCommandPalette`. Defaults to `"#333333"`.
----@field command_palette_fg_color string Specifies the text color used by
----       `ActivateCommandPalette`. Defaults to `rgba(0.75, 0.75, 0.75, 1.0)`.
----@field command_palette_font_size integer Specifies the size of the font used with
----       `ActivateCommandPalette`. Defaults to `14`.
----@field char_select_bg_color string Specifies the background color used by `CharSelect`.
----       Defaults to `"#333333"`.
----@field char_select_fg_color string Specifies the foreground color used by `CharSelect`.
----       Defaults to `rgba(0.75, 0.75, 0.75, 1.0)`.
----@field char_select_font_size string Specifies the size of the font used with `CharSelect`.
----       Defaults to `14`
 local config = {}
 
 ---Set options for bg, fg and font size for both command-palette and char-select
