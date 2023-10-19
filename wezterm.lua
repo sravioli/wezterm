5c0960065475f6cf7c4ba7a1a4ddf9342561fdb6
Simone Fidanza <fsimone2002@proton.me>
2023-10-19 09:02:22 +0200

    feat(wezterm): unload old config modules

--- wezterm.lua
+++ wezterm.lua
@@ -4,5 +4,5 @@ return Config:init()
   :add(require "config.window")
   :add(require "config.appearance")
   :add(require "config.cursor")
-  :add(require "config.command-palette")
   :add(require "config.bell").options
+-- :add(require "config.command-palette")
