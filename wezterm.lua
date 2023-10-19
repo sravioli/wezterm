3279fd102f301564855ed504a59deb4629560045
Simone Fidanza <fsimone2002@proton.me>
2023-10-19 13:45:18 +0200

    feat(wezterm): load additional modules

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,11 @@
 local Config = require "config.init"
 
 return Config:init()
+  :add(require "config.gpu")
+  :add(require "config.font")
   :add(require "config.window")
-  :add(require "config.appearance")
   :add(require "config.cursor")
+  :add(require "config.appearance")
+  :add(require "config.exit-behavior")
   :add(require "config.bell").options
 -- :add(require "config.command-palette")
