cb876ce8a40d509a9956154ffc0b1c8d7bf8f9fa
Simone Fidanza <fsimone2002@proton.me>
2023-10-22 15:38:46 +0200

    feat(events:format-window-title): custom format window title

---
+++ events/format-window-title.lua
@@ -0,0 +1,31 @@
+local wez = require "wezterm" ---@class WezTerm
+local nf = require "utils.nerdfont-icons" ---@class NerdFontIcons
+local fn = require "utils.functions" ---@class UtilityFunctions
+
+local M = {}
+
+function M.setup()
+  wez.on("format-window-title", function(tab, pane, tabs, _, _)
+    local zoomed = ""
+    if tab.active_pane.is_zoomed then zoomed = "[Z] " end
+
+    local index = ""
+    if #tabs > 1 then index = string.format("[%d/%d] ", tab.tab_index + 1, #tabs) end
+
+    ---tab title
+    local title = fn.basename(pane.title):gsub("%.exe%s?$", "")
+
+    -- HACK: running Neovim will turn the tab title to "C:\WINDOWS\system32\cmd.exe".
+    -- This is indeed a hack, but I'm never running cmd.exe so it's safe to override
+    -- this way.
+    if title == "cmd" then
+      title = "Neovim"
+        .. string.format(" %s%s%s", "(dir: ", fn.basename(pane.current_working_dir), ")")
+    end
+
+    -- return zoomed .. index .. tab.active_pane.title
+    return zoomed .. index .. title
+  end)
+end
+
+return M
