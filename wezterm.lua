3ad7119ee008f9822b4776acb4d6ec518553107d
Simone Fidanza <fsimone2002@proton.me>
2023-12-14 09:01:44 +0100

    feat(wezterm.lua): load new config

--- wezterm.lua
+++ wezterm.lua
@@ -1,18 +1,15 @@
-local Config = require "config.init"
+---@class Config
+local Config = require("utils.config"):new()
 
-require("events.format-tab-title").setup()
-require("events.format-window-title").setup()
-require("events.update-status").setup()
-require("events.new-tab-button-click").setup()
+require "events.update-status"
+require "events.format-tab-title"
+require "events.new-tab-button-click"
 
-return Config:init()
-  :add(require "config.gpu")
-  :add(require "config.font")
-  :add(require "config.bell")
-  :add(require "config.window")
-  :add(require "config.cursor")
-  :add(require "config.general")
+return Config:add(require "config.general")
   :add(require "config.appearance")
-  :add(require "config.exit-behavior")
-  :add(require "keys.tables")
-  :add(require "keys.bindings").options
+  :add(require "config.tab-bar")
+  :add(require "config.font")
+  :add(require "config.gpu")
+  :add(require "mappings.default")
+  :add(require "mappings.modes")
+
