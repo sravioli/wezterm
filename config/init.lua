1c7d50a119b84e47d40ec63537c0b0422aadc04a
Simone Fidanza <fsimone2002@proton.me>
2023-12-16 19:39:06 +0100

    feat: merge config in config and mappings folder

---
+++ config/init.lua
@@ -0,0 +1,8 @@
+return require("utils.fun").tbl_merge(
+  (require "config.gpu"),
+  (require "config.appearance"),
+  (require "config.font"),
+  (require "config.tab-bar"),
+  (require "config.general")
+)
+
---
+++ mappings/init.lua
@@ -0,0 +1,5 @@
+return require("utils.fun").tbl_merge(
+  (require "mappings.default"),
+  (require "mappings.modes")
+)
+
