70181bb0f567f80c6617d9cfd3cc8a26552d161e
sravioli <fsimone2002@proton.me>
2024-01-24 12:49:20 +0100

    feat(wezterm.lua): source new event

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,7 @@
 require "events.update-status"
 require "events.format-tab-title"
 require "events.new-tab-button-click"
+require "events.lock-interface"
 
 return require("utils.config"):new():add("config"):add "mappings"
 
