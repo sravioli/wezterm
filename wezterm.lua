44e204367db52214eb09e1e87cd4158d3cb91196
sravioli <sravioli+github@proton.me>
2024-05-16 20:30:49 +0200

    feat(wezterm.lua): load new event

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,6 @@ require "events.update-status"
 require "events.format-tab-title"
 require "events.new-tab-button-click"
 require "events.lock-interface"
+require "events.augment-command-palette"
 
 return require("utils.config"):new():add("config"):add "mappings"
-
