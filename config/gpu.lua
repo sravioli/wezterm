11ebe6701c578b0bdb2f8288f6e6bb42fcbe0ea2
sravioli <sravioli+github@proton.me>
2024-07-01 23:16:34 +0200

    chore: use the gpu picker

--- config/gpu.lua
+++ config/gpu.lua
@@ -4,15 +4,6 @@ local Config = {}
 Config.front_end = "WebGpu"
 Config.webgpu_force_fallback_adapter = false
 Config.webgpu_power_preference = "HighPerformance"
-Config.webgpu_preferred_adapter = {
-  backend = "Vulkan",
-  device = 8081,
-  device_type = "DiscreteGpu",
-  driver = "NVIDIA",
-  driver_info = "537.58",
-  name = "NVIDIA GeForce GTX 1650 with Max-Q Design",
-  vendor = 4318,
-}
+Config.webgpu_preferred_adapter = require("utils.gpu_adapter"):pick_best()
 
 return Config
-
