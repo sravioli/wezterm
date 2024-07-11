47bde614c30767204f42d3d66a8dff392510cc1e
sravioli <sravioli+github@proton.me>
2024-07-11 22:44:34 +0200

    chore(wezterm.lua): formatting

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,10 @@
+---@class Config
+local Config = require("utils.config"):new()
+
 require "events.update-status"
 require "events.format-tab-title"
 require "events.new-tab-button-click"
 require "events.lock-interface"
 require "events.augment-command-palette"
 
-return require("utils.config"):new():add("config"):add "mappings"
+return Config:add("config"):add "mappings"
