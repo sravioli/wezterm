9b25d7a22a3ff0ae6503a2d7cb1de268c360b8e0
sravioli <sravioli+github@proton.me>
2024-07-20 01:23:43 +0200

    fix(config:gpu): extract battery info to variable

--- config/gpu.lua
+++ config/gpu.lua
@@ -1,11 +1,12 @@
----@class Config
+local battery_charge = require("wezterm").battery_info()[1].state_of_charge
+
 local Config = {}
 
 Config.front_end = "WebGpu"
 Config.webgpu_force_fallback_adapter = false
 
 ---switch to low power mode when battery is low
-if require("wezterm").battery_info()[1].state_of_charge < 0.35 then
+if battery_charge < 0.35 then
   Config.webgpu_power_preference = "LowPower"
 else
   Config.webgpu_power_preference = "HighPerformance"
