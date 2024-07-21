0bce154266db69b5f3159323f0372763c9bf0b18
sravioli <sravioli+github@proton.me>
2024-07-21 19:17:59 +0200

    fix(wezterm.lua): disable format-tab-title event due to performance

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@
 local Config = require("utils.class.config"):new()
 
 require "events.update-status"
-require "events.format-tab-title"
+-- require "events.format-tab-title"
 require "events.new-tab-button-click"
 require "events.augment-command-palette"
 
