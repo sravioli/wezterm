db93543229c484c597f349e000b807c68a301b20
sravioli <sravioli+github@proton.me>
2024-07-25 19:46:23 +0200

    feat(pick-lists:font-sizes): move to pick-lists folder, adapt to new picker

---
+++ pick-lists/font-sizes/font-sizes.lua
@@ -0,0 +1,18 @@
+---@class PickList
+local M = {}
+
+M.get = function()
+  local sizes = {}
+  for i = 8, 30 do
+    sizes[#sizes + 1] = { label = ("%2dpt"):format(i), id = tostring(i) }
+  end
+  sizes[#sizes + 1] = { id = tostring(require("config.font").font_size), label = "Reset" }
+
+  return sizes
+end
+
+M.activate = function(Config, opts)
+  Config.font_size = tonumber(opts.id)
+end
+
+return M
