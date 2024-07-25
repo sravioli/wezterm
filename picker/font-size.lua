3aa8422ccb3b958d3bbff7b8996bcb8a08a3a5ff
sravioli <sravioli+github@proton.me>
2024-07-25 19:45:32 +0200

    fix(picker): reimplement pickers with the new class

--- picker/font-size.lua
+++ picker/font-size.lua
@@ -1,47 +1,17 @@
----@diagnostic disable: undefined-field
 local wt = require "wezterm"
-local log_info = wt.log_info
-local sizes = require "sizes.sizes"
 
-local FontSizePicker = require("picker.generic"):init()
+local Utils = require "utils"
+local Picker = Utils.class.picker
+local fs = Utils.fn.fs
 
-FontSizePicker.title = "Font Size Picker"
-FontSizePicker.fuzzy = true
-FontSizePicker.choices = {}
-
-local size_options = sizes.init()
-
--- Create choices
-for _, size in ipairs(size_options) do
-  local label, value
-  if type(size) == "table" then
-    label, value = size.label, size.value
-  else
-    label, value = size, size
-  end
-
-  FontSizePicker.choices[#FontSizePicker.choices + 1] = {
-    label = label,
-    id = tostring(value),
-  }
-end
-
-FontSizePicker.action = function(window, _, id, label)
-  if not id and not label then
-    return log_info "Font size picking cancelled"
-  end
-
-  log_info("Applying font size: ", label)
-  local Overrides = window:get_config_overrides() or {}
-
-  if id == "Reset" then
-    Overrides.font_size = nil
-  else
-    local value = id ~= "Reset" and tonumber(id) or id
-    sizes.activate(Overrides, nil, value)
-  end
-
-  window:set_config_overrides(Overrides)
-end
+local FontSizePicker = Picker.new {
+  title = "Font size picker",
+  subdir = fs.pathconcat "font-sizes",
+  fuzzy = true,
+  comp = function(a, b)
+    local label = "Reset"
+    return (a.label == label) or (b.label ~= label and a.label < b.label)
+  end,
+}
 
 return FontSizePicker
