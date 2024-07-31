65e88243a7bb7800eb4ac15412bdcbb1ff12c59b
sravioli <sravioli+github@proton.me>
2024-08-01 00:54:38 +0200

    feat(events:augment-command-palette): add pickers to command palette

--- events/augment-command-palette.lua
+++ events/augment-command-palette.lua
@@ -21,5 +21,25 @@ wt.on("augment-command-palette", function(_, _)
         end),
       },
     },
+    {
+      brief = "Colorscheme picker",
+      icon = "md_palette",
+      action = require("picker.colorscheme"):pick(),
+    },
+    {
+      brief = "Font picker",
+      icon = "md_format_font",
+      action = require("picker.font"):pick(),
+    },
+    {
+      brief = "Font size picker",
+      icon = "md_format_font_size_decrease",
+      action = require("picker.font-size"):pick(),
+    },
+    {
+      brief = "Font leading picker",
+      icon = "fa_text_height",
+      action = require("picker.leading"):pick(),
+    },
   }
 end)
