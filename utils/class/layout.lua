d87355b4f968c92dd4d80cc040afe2c96019fe66
sravioli <sravioli+github@proton.me>
2024-08-03 12:48:55 +0200

    fix(utils:layout): implement missing colors, add logging

--- utils/class/layout.lua
+++ utils/class/layout.lua
@@ -1,14 +1,9 @@
----@diagnostic disable: undefined-field
-
----Utility class to simplify the creation and handling of wezterm's `FormatItem` object.
----
 ---@module "utils.class.layout"
 ---@author sravioli
 ---@license GNU-GPLv3.0
 
+local Logger = require "utils.class.logger"
 local wt = require "wezterm"
-local log_info, log_error = wt.log_info, wt.log_error
-local wt_format = wt.format
 
 local attribute_mappings = {
   None = "ResetAttributes",
@@ -25,53 +20,88 @@ local attribute_mappings = {
   NoItalic = { Italic = false },
 }
 
+-- stylua: ignore
+local ansi_colors = {
+  Black = true, Maroon  = true, Green = true, Olive = true, Navy = true, Purple = true,
+  Teal  = true, Silver  = true, Grey  = true, Red   = true, Lime = true, Yellow = true,
+  Blue  = true, Fuchsia = true, Aqua  = true, White = true,
+}
+
+---The `Layout` class was created to address the need for a more straightforward and organized
+---way to build formatted text layouts in Wezterm.  Wezterm's
+---[`FormatItem`](https://wezfurlong.org/wezterm/config/lua/wezterm/format.html) objects
+---can be cumbersome to handle directly, especially when dealing with multiple attributes
+---and text segments.  The `Layout` class encapsulates this complexity, providing a clear
+---and concise API to manage these objects.
+---
 ---@class Utils.Class.Layout
+---@field private layout table
+---@field private log    table
+---@field private name   string
 local M = {}
 
----@diagnostic disable-next-line: duplicate-doc-alias
----@alias Layout Utils.Class.Layout
-
----Creates a new instance of the Layout class.
+---Creates a new class instance.
 ---
----Initializes a new Layout object with an empty layout table.
----
----@return Layout layout newly created class instance.
-function M:new()
-  return setmetatable({ layout = {} }, { __index = self })
+---@param name? string name of the layout. Used for logging purposes.
+---@return Utils.Class.Layout layout newly created class instance.
+function M:new(name)
+  name = "Layout" .. (name and " > " .. name or "")
+  return setmetatable(
+    { layout = {}, log = Logger:new(name), name = name },
+    { __index = self }
+  )
 end
 
----Add elements to the layout table.
+---Push the given element in the Layout.
 ---
----Adds text elements to the layout with specified background and foreground colors, as
----well as optional attributes such as underline style, intensity, and italic style.
+---Pushes the text element with the specified background and foreground colors, as well as
+---optional attributes such as: underline style, text intensity, etc.
 ---
----The following attributes are supported:
+---Supports all the attributes that the Wezterm's
+---[`FormatItem`](https://wezfurlong.org/wezterm/config/lua/wezterm/format.html) object
+---defines:
 ---
----```lua
----local attributes = {
----  None = "ResetAttributes",
----  NoUnderline = { Underline = "None" },
----  Single = { Underline = "Single" },
----  Double = { Underline = "Double" },
----  Curly = { Underline = "Curly" },
----  Dotted = { Underline = "Dotted" },
----  Dashed = { Underline = "Dashed" },
----  Normal = { Intensity = "Normal" },
----  Bold = { Intensity = "Bold" },
----  Half = { Intensity = "Half" },
----  Italic = { Italic = true },
----  NoItalic = { Italic = false },
----}
----```
----@param background string background color of the cell.
----@param foreground string foreground color of the cell.
----@param text string text to be added.
----@param attributes? table list of attributes to be added.
----@return Layout self The updated layout instance.
+--- - `None`: Reset attributes.
+--- - `NoUnderline`: No underline.
+--- - `Single`: Single underline.
+--- - `Double`: Double underline.
+--- - `Curly`: Curly underline.
+--- - `Dotted`: Dotted underline.
+--- - `Dashed`: Dashed underline.
+--- - `Normal`: Normal intensity.
+--- - `Bold`: Bold intensity.
+--- - `Half`: Half intensity.
+--- - `Italic`: Italic text.
+--- - `NoItalic`: No italic text.
+---
+---Moreover support the definition of the `background` and/or `foreground` color either
+---with ansi colors, named colors, rbg values or Wezterm's color objects.
+---
+---@param background  string background color of the element.
+---@param foreground  string foreground color of the element.
+---@param text        string text to be added.
+---@param attributes? table  attributes to style the text.
+---@return Utils.Class.Layout self updated layout instance.
 function M:push(background, foreground, text, attributes)
+  self.log:debug(
+    "pushing: { bg: %s, fg: %s, attributes: %s, text: %s }",
+    background,
+    foreground,
+    attributes,
+    text
+  )
   self = self or {}
-  self[#self + 1] = { Background = { Color = background } }
-  self[#self + 1] = { Foreground = { Color = foreground } }
+
+  local function set_color(attr, color)
+    if ansi_colors[color] then
+      self[#self + 1] = { [attr] = { AnsiColor = color } }
+    else
+      self[#self + 1] = { [attr] = { Color = color } }
+    end
+  end
+
+  set_color("Background", background)
+  set_color("Foreground", foreground)
 
   if attributes then
     for k = 1, #attributes do
@@ -79,7 +109,7 @@ function M:push(background, foreground, text, attributes)
       if attribute_mappings[attribute] then
         self[#self + 1] = { Attribute = attribute_mappings[attribute] }
       else
-        log_error("Layout: Attribute '" .. attribute .. "' is non-existant")
+        self.log:error("attribute '%s' is not defined!", attribute)
       end
     end
   end
@@ -91,8 +121,9 @@ end
 
 ---Clears all elements from the layout.
 ---
----@return Layout self The cleared layout instance.
+---@return Utils.Class.Layout self The cleared layout instance.
 function M:clear()
+  self.log:debug("clearing layout %s", self.name)
   self.layout = {}
   return self
 end
@@ -101,13 +132,16 @@ end
 ---
 ---@return string format The resulting string formatted by `wezterm.format()`
 function M:format()
-  return wt_format(self)
+  self.log:debug("formatting layout %s", self.name)
+
+  ---@diagnostic disable-next-line: undefined-field
+  return wt.format(self)
 end
 
 ---Logs the unformatted layout to wezterm's debug console
 ---@param formatted boolean whether to log the formatted layout or not
 function M:debug(formatted)
-  log_info { Layout = formatted and self:format() or self }
+  self.log:info(formatted and self:format() or self)
 end
 
 return M
