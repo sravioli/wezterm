ba524ab9f644fdf507bcbeba544e09e63e2fe3ff
Simone Fidanza <fsimone2002@proton.me>
2024-11-11 09:58:41 +0100

    feat: add keymap to select new tab to open

--- mappings/default.lua
+++ mappings/default.lua
@@ -39,6 +39,14 @@ local mappings = {
   { "<C-S-Insert>", act.PasteFrom "PrimarySelection", "" },
   { "<C-Insert>", act.CopyTo "PrimarySelection", "" },
   { "<C-S-Space>", act.QuickSelect, "quick select" },
+  {
+    "<S-M-t>",
+    act.ShowLauncherArgs {
+      title = "  Search:",
+      flags = "FUZZY|LAUNCH_MENU_ITEMS|DOMAINS",
+    },
+    "new window",
+  },
 
   ---quick split and nav
   { '<C-S-">', act.SplitHorizontal { domain = "CurrentPaneDomain" }, "vsplit" },
