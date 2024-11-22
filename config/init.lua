fa1ef3592a363bf51224eeb49b08aaa992c9eb10
sravioli <sravioli+github@proton.me>
2024-11-23 00:10:34 +0100

    feat: use new utils.fn.tbl.merge() function

--- config/init.lua
+++ config/init.lua
@@ -1,7 +1,7 @@
-return require("utils.fn").tbl_merge(
-  (require "config.gpu"),
+return require("utils.fn").tbl.merge(
   (require "config.appearance"),
   (require "config.font"),
   (require "config.tab-bar"),
-  (require "config.general")
+  (require "config.general"),
+  (require "config.gpu")
 )
