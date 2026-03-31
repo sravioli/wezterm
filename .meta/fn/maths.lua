aafb0ec99f12b48bda0ca337a4c7a51964aa913b
sravioli <s.fidanza@pm.me>
2026-03-31 17:27:34 +0200

    refactor: remove unused meta files from the project

--- .meta/fn/maths.lua
+++
@@ -1,6 +0,0 @@
----@meta fn.Maths
-error "cannot require a meta file!"
-
----Numeric rounding and conversion utilities.
----
----@class Fn.Maths
--- .meta/opts/utils/base.lua
+++
@@ -1,9 +0,0 @@
----@meta Opts.Utils.Base
-error "cannot require a meta file!"
-
--- luacheck: push ignore 631 (line is too long)
-
----@class Opts.Utils.Base
----@field public log? Opts.Utils.Logger
-
--- luacheck: pop
